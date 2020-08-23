faa230b3af5997da7c8df96c243227d67b49e4dc
AndreiBucur <51850157+AndreiBucur@users.noreply.github.com>
2020-08-23 14:09:08 +0300

    dio

--- js/script.js
+++ js/script.js
@@ -11,16 +11,22 @@ element.appendChild(renderer.domElement);
 
 function startup() {
   controls = new THREE.DeviceOrientationControls( camera );
-  controls.enable = true
+  controls.enabled = true
   var imgCircleGeometry = new THREE.SphereGeometry(2, 15, 15);
   var imgCircleMaterial = new THREE.MeshBasicMaterial({ color: 0x09AC8D });
   var imgCircle = new THREE.Mesh(imgCircleGeometry, imgCircleMaterial);
-  scene.add(imgCircle); // add Mercury
+  scene.add(imgCircle);
+  //--
+  var imgCrcleGeometry = new THREE.SphereGeometry(5, 15, 15);
+  var imgCrcleMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.BackSide, wireframe: true });
+  var imgCrcle = new THREE.Mesh(imgCrcleGeometry, imgCrcleMaterial);
+  scene.add(imgCrcle);
+  //--
 
   var t = 0;
   function render() {
     requestAnimationFrame(render);
-    t += 0.01;
+    t += 0.05;
 
     imgCircle.position.x = 20 * Math.cos(t) + 0;
     imgCircle.position.z = 20 * Math.sin(t) + 0;
