8e0858940cab25f59decb1a958bda0413a901669
AndreiBucur <51850157+AndreiBucur@users.noreply.github.com>
2020-09-18 20:47:35 +0300

    gdgbggg
    gggg

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // info
-/*
-infoX = document.getElementById("xinfo");
-infoY = document.getElementById("yinfo");
-infoZ = document.getElementById("zinfo");*/
+
+const infoX = document.getElementById("xinfo");
+const infoY = document.getElementById("yinfo");
+const infoZ = document.getElementById("zinfo");
 
 var element = document.getElementById('container')
 var renderer = new THREE.WebGLRenderer();
@@ -23,8 +23,6 @@ function startup() {
   var loader = new THREE.TextureLoader();
   loader.load('exampleVR1.jpg', function (texture) {
 
-    var geometry = new THREE.SphereGeometry(200, 20, 20);
-
     var imgCrcleGeometry = new THREE.SphereGeometry(5, 15, 15);
     var imgCrcleMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: texture, side: THREE.BackSide, });
     var imgCrcle = new THREE.Mesh(imgCrcleGeometry, imgCrcleMaterial);
@@ -35,27 +33,7 @@ function startup() {
   var imgCircleMaterial = new THREE.MeshBasicMaterial({ color: 0x09AC8D });
   var imgCircle = new THREE.Mesh(imgCircleGeometry, imgCircleMaterial);
   scene.add(imgCircle);
-  /*
-    if (window.DeviceOrientationEvent) {
-  
-      window.addEventListener("deviceorientation", function (event) {
-  
-        camera.rotation.x = event.gamma * Math.PI;
-        camera.rotation.y = event.beta * Math.PI;
-        camera.rotation.z = event.alpha * Math.PI;
-  
-        infoX.innerHTML = event.gamma;
-        infoY.innerHTML = event.beta;
-        infoZ.innerHTML = event.alpha;
-  
-      }, true);
-  
-  
-  
-    } else {
-      alert("Sorry, your browser doesn't support Device Orientation");
-    }
-  */
+
   const controls = new DeviceOrientationControls(camera);
   controls.update();
 
@@ -63,11 +41,11 @@ function startup() {
     requestAnimationFrame(render);
     renderer.render(scene, camera);
     controls.update();
-    /*
+
     infoX.innerHTML = camera.rotation.x;
     infoY.innerHTML = camera.rotation.y;
     infoZ.innerHTML = camera.rotation.z;
-    */
+
   }
   render();
 }
